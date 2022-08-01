e021966840e72bf1ea164cbae87182e7db822aa1
Sergio Paniagua Sanchez <pani77sps@gmail.com>
2022-08-01 18:23:50 +0200

    Ejercicio 3
    Implementar y testear función `moviesAverageOfDirector( )`

--- src/films.js
+++ src/films.js
@@ -1,9 +1,9 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  const DIRECTORS = array.map((x) => x.director);
+  const directors = array.map((x) => x.director);
 
-  console.log('EXERCICE 1 ->', DIRECTORS);
-  return DIRECTORS;
+  console.log('EXERCICE 1 ->', directors);
+  return directors;
 }
 
 // Exercise 2: Get the films of a certain director
@@ -15,7 +15,19 @@ function getMoviesFromDirector(array, director) {
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
-function moviesAverageOfDirector(array, director) {}
+function moviesAverageOfDirector(array, director) {
+  const DIRECTOR = array.filter((item) => item.director === director);
+
+  const totalScore = DIRECTOR.reduce((total, item) => {
+    return (total = total + item.score);
+  }, 0);
+
+  // toFixed() devuelve 'string', por eso convertimos a number con el +()
+  let average = +(totalScore / DIRECTOR.length).toFixed(2);
+
+  console.log('EXERCISE 3 ->', average);
+  return average;
+}
 
 // Exercise 4:  Alphabetic order by title
 function orderAlphabetically(array) {}
